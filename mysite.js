c1ef6102913fac34e9ef5bd3d6d1f6d496629ad0
JazzSteve <63267630+JazzSteve@users.noreply.github.com>
2020-05-11 21:24:26 -0400

    initial commit

---
+++ mysite.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+ $('.header').height($(window).height());  //header height
+ 
+})
+
+//NavBar Scroll Change
+
+/*$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 50) {
+	    $("#mainnavbar").css("background" , "#c2ae4c");
+	  }
+
+	  else{
+		  $("#mainnavbar").css("background" , "#1a1a19");  	
+	  }
+  })
+})*/
